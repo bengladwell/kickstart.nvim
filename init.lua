801faae0943025cf443d47da0924826b883f1220
Ben Gladwell <bengladwell@gmail.com>
2024-11-23 12:47:55 -0500

    Add buffer-based completion

--- init.lua
+++ init.lua
@@ -1012,6 +1012,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
@@ -1082,6 +1083,7 @@ require('lazy').setup({
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'buffer' },
           { name = 'path' },
         },
       }
