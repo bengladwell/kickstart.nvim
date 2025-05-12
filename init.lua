2cb61ae53072b4d1c5bfabc983b69fc7595bf6f0
Ben Gladwell <bgladwell@swoop.com>
2025-05-12 10:17:30 -0400

    Update pyright/uv compatibility

--- init.lua
+++ init.lua
@@ -1043,7 +1043,14 @@ require('lazy').setup({
         --   "venv": "astrology"
         -- }
 
-        pyright = {},
+        -- See https://www.andersevenrud.net/neovim.github.io/lsp/configurations/pyright/
+        pyright = {
+          settings = {
+            python = {
+              pythonpath = vim.fn.getcwd() .. '/.venv/bin/python',
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
